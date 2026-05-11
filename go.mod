5532edb23b3cad841b283cac4fcda8c66c3f9720
waiyneee <dikshkaushik1111@gmail.com>
2026-05-11 13:06:37 +0530

    Initialized the project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/waiyneee/Kvstore
+
+go 1.26.1
---
+++ main.go
@@ -0,0 +1,11 @@
+package main 
+
+
+import "fmt"
+
+func main(){
+
+
+	fmt.Println("Started entry point for Kvstores initialization ")
+
+}
