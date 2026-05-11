ee8dfd30e8ed18e70601af4dce48f32a8ceadcd3
waiyneee <dikshkaushik1111@gmail.com>
2026-05-11 13:44:45 +0530

    basic sockets tcp server progrmming

--- main.go
+++ main.go
@@ -1,11 +1,48 @@
-package main 
+package main
 
-
-import "fmt"
+import (
+	"fmt"
+	"net"
+	"os"
+)
 
 func main(){
 
 
-	fmt.Println("Started entry point for Kvstores initialization ")
+	fmt.Println("Started entry poitn for Kvstores initialization ")
+
+
+
+	//go run main.go port(6379 preferably)
+	if len(os.Args)<2{
+		//failure 
+		//use log instaed of printff later on 
+		fmt.Printf("we got an unexpected length of args here ")
+		//telling os to exit 
+		os.Exit(1)
+
+
+	}
+
+	//LETS STARTS
+	port:=fmt.Sprintf(":%s",os.Args[1])
+
+	lstnr,err:=net.Listen("tcp",port)
+	if err!=nil{
+		fmt.Println("got listening error",err)
+		os.Exit(1)
+
+
+	}
+
+	defer lstnr.Close()
+	// fmt.Printf("listenign to :%s\n",lstnr.Addr())
+
+	// for{
+	// 	conn,err:=lstnr.Accept()
+
+	// }
+
+
 
 }
